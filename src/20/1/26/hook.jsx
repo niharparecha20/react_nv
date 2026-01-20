61dbbae7f006c0078eda6a85b97043b3ce5892f9
Nihar <parechanihar@gmail.com>
2026-01-20 09:03:16 +0530

    add on hook

--- src/20/1/26/hook.jsx
+++ src/20/1/26/hook.jsx
@@ -2,12 +2,19 @@ import React,{useState} from "react";
 
 export default function HookExample(){
     const[count,setCount] =useState(0);
+    const[name, setName] = useState("guest")
+    const[checked,setChecked] = useState(false)
     return(
         <div>
+            <input type="checkbox" checked = {checked} onChange={()=>setChecked(!checked)}
+            />
             <h1>Count: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>decerement</button>
+            <h1>Name:{name}</h1>
+            <input type ="text"value={name}onChange={(e)=>setName(e.target.value)}/>{" "}
             
         </div>
     )
 }
+//all the hooks are function 
