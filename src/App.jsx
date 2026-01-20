938573383b0ec58cbb6751002e9179c97a38d6c6
Nihar <parechanihar@gmail.com>
2026-01-20 09:21:31 +0530

    hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import MultiObjects from "./150126/multiobject";
 import multiobject from "./150126/multiobject"; 
 import HookExample from "./20/1/26/hook";
+import Hookother from "./20/1/26/hook2";
 function App() {
 
   return (
     <>
     {/*<MultiObjects/>*/}
-    <HookExample/>
+    <Hookother/>
 
       
    </>
