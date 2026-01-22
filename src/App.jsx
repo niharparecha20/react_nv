a7d4704578bf4d7c322eaddef3cb1adbb7042f28
Nihar <parechanihar@gmail.com>
2026-01-22 20:32:25 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import MultiObjects from "./150126/multiobject";
-import multiobject from "./150126/multiobject"; 
-import HookExample from "./20/1/26/hook";
-import Hookother from "./20/1/26/hook2";
+// import MultiObjects from "./150126/multiobject";
+// import multiobject from "./150126/multiobject"; 
+// import HookExample from "./20/1/26/hook";
+// import Hookother from "./20/1/26/hook2";
+ //import useState3 from "./210126/useState3";
+ import UseState3 from "./210126/useState3";
 function App() {
 
   return (
     <>
     {/*<MultiObjects/>*/}
-    <Hookother/>
+    <UseState3/>
 
       
    </>
