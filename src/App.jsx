c509a528d3c496644dc674262fa961444d8da880
Nihar <parechanihar@gmail.com>
2026-02-12 08:45:13 +0530

    comt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+<<<<<<< HEAD
 import React from "react";
 import ShoppingCart from "./10226/shoppingcart";
 
+=======
+// import MultiObjects from "./150126/multiobject";
+// import multiobject from "./150126/multiobject"; 
+// import HookExample from "./20/1/26/hook";
+// import Hookother from "./20/1/26/hook2";
+ //import useState3 from "./210126/useState3";
+ import UseState3 from "./210126/useState3";
+>>>>>>> 077bd0f0dc82a066c34aa197c5c46e3d3afbd091
 function App() {
   return (
+<<<<<<< HEAD
     <div className="ShoppingCartApp">
       <ShoppingCart />
     </div>
+=======
+    <>
+    {/*<MultiObjects/>*/}
+    <UseState3/>
+
+      
+   </>
+>>>>>>> 077bd0f0dc82a066c34aa197c5c46e3d3afbd091
   );
 }
 
